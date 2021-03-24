1b8d44f8c006412f2e45ca7f7b846a161bac1b8a
Jongwon Park <pjw970220@gmail.com>
2021-03-24 14:12:00 +0900

    Week4 Init

---
+++ report/Week4/exam/ex_4_2_start/ex_4_2_start.ino
@@ -0,0 +1,24 @@
+/*
+ 예제 4.2
+ LED 밝기 조절
+*/
+
+const int ledA   =  3;	  //LED A를 3번핀에 연결
+const int ledB   =  5;	  //LED B를 5번핀에 연결
+       //밝기를 조절하기 위한 변수
+       //밝기 변수 증감을 위한 변수
+
+void setup()
+{
+// analogWrite 핀에는 별도의 설정이 불필요하다.
+}
+
+void loop()
+{
+    // LED A 밝기 조절
+    // LED B 밝기 조절
+
+  	// 밝기 조절
+    // 밝기 변수 증감 방향 변경
+  delay(10);    // 0.01 초간 지연
+}
